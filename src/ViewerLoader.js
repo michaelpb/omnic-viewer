dd7724c3e5b63dccdb4588a7e056c5b70547391f
michaelb <michaelpb@gmail.com>
2017-10-25 11:15:19 -0700

    moving things around

--- src/ViewerLoader.js
+++ src/ViewerLoader.js
@@ -2,9 +2,8 @@
     Singleton for loading viewers
 */
 
-import PdfViewer from './components/viewers/PdfViewer';
-import StlViewer from './components/viewers/StlViewer';
-import FileViewerWrapper from './components/viewers/FileViewerWrapper';
+import PdfViewer from './components/viewers/pdf/PdfViewer';
+import StlViewer from './components/viewers/stl/StlViewer';
 
 export default class ViewerLoader {
     constructor() {
@@ -16,17 +15,6 @@ export default class ViewerLoader {
                 return PdfViewer;
             case 'STL':
                 return StlViewer;
-            /*
-                case 'JPEG':
-                case 'PDF':
-                case 'CSV':
-                case 'XLSX':
-                case 'DOCX':
-                case 'MP4':
-                case 'WEBM':
-                case 'MP3':
-                    return FileViewerWrapper;
-            */
             default:
                 return null;
         }
